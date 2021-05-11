03ccc0eb3c4f2c1fa5d61b9ee4aeab956ca2b2b7
Jure Mihevc <juremihevcster@gmail.com>
2021-05-11 19:56:29 +0200

    Heroku prep

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Message from the handler!"
+    current_year = datetime.datetime.now().year
+
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
 @app.route("/about")
 def about_me():
